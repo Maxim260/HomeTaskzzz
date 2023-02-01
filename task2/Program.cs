7b5384f9dc34e433cea7b9bcc387f2f5ab0494f0
Максим <maximka9i@vk.com>
2023-02-01 15:31:55 +1000

    classwork

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Console.Clear();
+Console.WriteLine("Hello, World!");*/
+// int n = int.Parse(Console.ReadLine());
+// int a=1, b=2;
+// Console.WriteLine($"{a},{b}");
+//Console.WriteLine($"Результат: {n * 3}");
+
+Console.Clear();
+
+Console.Write("Введите 1-ое число  ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число  ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+Console.WriteLine(n);
+else if (n < m)
+Console.WriteLine(m);
+else
+    Console.WriteLine("=");

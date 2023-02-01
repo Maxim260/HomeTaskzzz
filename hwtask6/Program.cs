e0985599570520079ddd5f8cfc7ceeb27ff9f5ed
Максим <maximka9i@vk.com>
2023-02-01 17:19:28 +1000

    done task6

---
+++ hwtask6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Enter the number:  ");
+int i=int.Parse(Console.ReadLine());
+if(i%2==0)
+{
+Console.Write("ЧЁТНОЕ");
+Console.Read();
+}
+else
+{
+Console.Write("НЕЧЁТНОЕ");
+Console.Read();
+}

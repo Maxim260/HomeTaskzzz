4f2e51ddf649e038f8a91b30372834b6cb84f19b
Максим <maximka9i@vk.com>
2023-02-01 17:38:43 +1000

    task2 complite

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,18 +6,18 @@
 // // // Console.WriteLine($"{a},{b}");
 // // //Console.WriteLine($"Результат: {n * 3}");
 
-// // Console.Clear();
+Console.Clear();
 
-// // Console.Write("Введите 1-ое число  ");
-// // int n = Convert.ToInt32(Console.ReadLine());
-// // Console.Write("Введите 2-ое число  ");
-// // int m = Convert.ToInt32(Console.ReadLine());
-// // if (n > m)
-// // Console.WriteLine(n);
-// // else if (n < m)
-// // Console.WriteLine(m);
-// // else
-// //     Console.WriteLine("=");
+Console.Write("Введите 1-ое число  ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число  ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+Console.WriteLine(n);
+else if (n < m)
+Console.WriteLine(m);
+else
+    Console.WriteLine("=");
 
 // Console.Clear();
 // Console.Write("Введите число:  ");

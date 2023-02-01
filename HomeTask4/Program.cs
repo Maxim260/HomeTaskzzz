17e6808d39c885bd2260d119731b4d59a1f749af
Максим <maximka9i@vk.com>
2023-02-01 16:47:06 +1000

    donetask4

---
+++ HomeTask4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("enter the first number:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number:  ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the third number:  ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+if (a>c)
+
+Console.WriteLine($"The number MAX: {a}");
+else
+Console.WriteLine($"The number MAX: {c}");
+else if (b>c)
+Console.WriteLine($"The number MAX: {b}");
+else 
+Console.WriteLine($"The number MAX: {c}");

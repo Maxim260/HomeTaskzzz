a7cd014a9949d488e38299b31ddc598758a3559f
Максим <maximka9i@vk.com>
2023-02-01 19:10:28 +1000

    t8

---
+++ hwtask8/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу,которая на вход принимает число (И),
+//а на выходе показывает все чётные числа от 1 до И.
+
+Console.Clear();
+Console.Write("Enter the number:  ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);

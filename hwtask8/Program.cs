0e01fb19c5d5930d8c4df49d072f2640ac5c204f
Максим <maximka9i@vk.com>
2023-02-02 14:09:09 +1000

    task8 was done with u help

--- hwtask8/Program.cs
+++ hwtask8/Program.cs
@@ -1,9 +1,9 @@
 ﻿//Напишите программу,которая на вход принимает число (И),
 //а на выходе показывает все чётные числа от 1 до И.
 
-Console.Clear();
-Console.Write("Enter the number:  ");
-int n = int.Parse(Console.ReadLine());
-for (int i = 1; i <= n; i++)
-    if (i % 2 == 0)
-        Console.WriteLine(i);
+        Console.Clear();
+        Console.Write("Enter the number:  ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 2; i <= n; i+=2)
+            // if (i % 2 == 0)
+                Console.Write($"{i}; ");

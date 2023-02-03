b66d2c8b9a0db116a38ec1d2101dab00e7153a3b
Максим <maximka9i@vk.com>
2023-02-03 16:24:50 +1000

    i wrote it task

---
+++ addtask/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Программа выводит второе значение по величине");
+Console.WriteLine("Вводите любые числа,а в конце введите 0");
+int n = Convert.ToInt32(Console.ReadLine()),max1 = n,max2 = 0;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max1)
+    {
+        max2 = max1;
+        max1 = n;
+    }
+    else if (n > max2)
+    max2 = n;
+}
+Console.WriteLine(max2);

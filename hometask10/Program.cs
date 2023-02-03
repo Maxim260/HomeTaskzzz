e1ca51f018687483b99642955eec49a370284369
Максим <maximka9i@vk.com>
2023-02-03 12:41:52 +1000

    done task10

---
+++ hometask10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Please, Enter a number from 100 to 999 ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a >= 100)
+a /= 10;
+int b = a % 10;
+Console.WriteLine($"Congratulations!!!You got a number): {b}");

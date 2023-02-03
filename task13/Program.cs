8e28ce11abf5ca6c44cf4d523fc557bec2e32f40
Максим <maximka9i@vk.com>
2023-02-03 13:35:47 +1000

    done task13

---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("ATTENTION!Программа выводит только третью цифру числа!");
+Console.Write("Enter a number here-> ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100)
+Console.WriteLine("В вашем числе нет третьей цифры:(.Попробуйте ещё ");
+else
+{
+while (a >= 999)
+{
+    a /= 10;
+}
+Console.WriteLine($"Congratulations!!!You got a number): {a % 10}");
+}

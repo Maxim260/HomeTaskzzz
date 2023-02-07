e5c9bf1da86c3604a64dac4b719b6add1230c901
Максим <maximka9i@vk.com>
2023-02-07 16:26:15 +1000

    done23

---
+++ 23task/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Home Work. Программа принимает число (N)
+и выдает таблицу кубов чисел от 1 до N.*/
+
+ Console.Clear();
+Console.WriteLine("Выдает таблицу кубов чисел от 1 до N ");
+Console.Write("Введите Number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+    Console.WriteLine($"{Math.Pow(i, 3)} ");

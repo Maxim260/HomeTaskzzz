7bbb569de567dcb4af93f82f483a5de2c2e50108
Максим <maximka9i@vk.com>
2023-02-08 17:51:27 +1000

    done with u help

---
+++ 36task/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 37. задайте одномерный массив заполненый случайными числами.
+найдите сумму элементов стоящих на нечётных позициях.*/
+
+void InputArray(int[] array)
+{
+    for (int i = 0;i < array.Length;i++)
+    array[i] = new Random().Next(100,1000);
+}
+
+int SummaOddInArray(int[] array)
+{
+    int summa = 0;
+    for(int i = 1; i < array.Length;i+=2)
+    summa = summa + array[i];
+    return summa;
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {SummaOddInArray(array)}");
+//всё равно ведь не смотрите те задания,которые сдают с опозданием..?

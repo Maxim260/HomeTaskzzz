d9b443bc1e81549aff339dd7eaad41f7edd4460f
Максим <maximka9i@vk.com>
2023-02-16 15:52:33 +1000

    fifti

---
+++ 60task/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.*/
+
+void InputMatrix(int[,,] ThreeMatrix)
+{
+    int number = 10;
+    for (int i = 0; i < ThreeMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < ThreeMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < ThreeMatrix.GetLength(2); k++)
+                ThreeMatrix[i, j, k] = number++; 
+        }
+    }
+}
+
+
+void PrintMatrix(int[,,] ThreeMatrix)
+{
+    for (int i = 0; i < ThreeMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < ThreeMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < ThreeMatrix.GetLength(2); k++)
+                Console.WriteLine($"{ThreeMatrix[i, j, k]} ({i}, {j}, {k})");
+        }
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размер трехмерного массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (size[0] * size[1] * size[2] > 90)
+{
+    Console.Write("Значения введены некорректные!\nВведите размер трехмерного массива: ");
+    size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+int[,,] ThreeMatrix = new int[size[0], size[1], size[2]];
+InputMatrix(ThreeMatrix);
+PrintMatrix(ThreeMatrix);
